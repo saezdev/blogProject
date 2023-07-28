5af741d3d2d815ad87bee44d9a2dc57d63b6ca73
Sergio <sergiosaezdev@gmail.com>
2023-07-28 04:57:11 +0200

    Added ondelete cascade to foreign keys

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     #Directly link an author to an authorization user
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
@@ -27,7 +27,7 @@ class Post(models.Model):
         return self.title
     
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post', related_name='comments')
+    post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
